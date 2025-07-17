15f03dedc06fdeccd9ae88bfafd83622072a8a70
Javier Azarola <jazarola87@gmail.com>
2025-07-17 11:36:04 +0000

    actualizando api

--- next.config.js
+++ next.config.js
@@ -21,7 +21,7 @@ const withPWA = require('next-pwa')({
 
 const nextConfig = {
   env: {
-    NEXT_PUBLIC_FIREBASE_API_KEY: "TU_API_KEY_AQUI",
+    NEXT_PUBLIC_FIREBASE_API_KEY: "AIzaSyD-NVeAE1XPsQxrQWvMo8XDfNDpvK4YT0o",
     NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: "aserradero-lhm-336e9.firebaseapp.com",
     NEXT_PUBLIC_FIREBASE_PROJECT_ID: "aserradero-lhm-336e9",
     NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: "aserradero-lhm-336e9.appspot.com",
