3bebfdfcb3f8e44250879e9605661509742f2926
Javier Azarola <jazarola87@gmail.com>
2025-07-17 10:47:57 +0000

    El archivo next.config.js tiene otros campos aparte de la api key sin co

---
+++ workspace/next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_FIREBASE_API_KEY: "TU_API_KEY_AQUI",
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: "aserradero-lhm-336e9.firebaseapp.com",
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: "aserradero-lhm-336e9",
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: "aserradero-lhm-336e9.appspot.com",
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: "1052943793792",
+    NEXT_PUBLIC_FIREBASE_APP_ID: "1:1052943793792:web:72828b615e85f061e38943",
+    NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: "G-G6509EV63K",
+  },
+};
+
+module.exports = nextConfig;
