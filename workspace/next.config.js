01ba4b748b636dbc2a1f1501c51d650f8a2b2053
Javier Azarola <jazarola87@gmail.com>
2025-07-17 11:52:31 +0000

    porque github me dice que la nueva api key esta expuesta?

--- workspace/next.config.js
+++ workspace/next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+  runtimeCaching: [
+    {
+      urlPattern: /^https?.*/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'offlineCache',
+        expiration: {
+          maxEntries: 200,
+        },
+      },
+    },
+  ],
+});
+
 const nextConfig = {
-  // Las variables de entorno ahora se gestionan en el archivo .env
+  // Las variables de entorno ahora se gestionan en el archivo .env.local
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
