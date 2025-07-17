a6c32389d28b59fa58e3d5b35a380218e4568c8a
Javier Azarola <jazarola87@gmail.com>
2025-07-17 13:37:29 +0000

    revisa y depura todo el directorio

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
-
-const {https} = require("firebase-functions");
-const {default: next} = require("next");
+const { https } = require("firebase-functions");
+const { default: next } = require("next");
 const path = require("path");
 
 const isDev = process.env.NODE_ENV !== "production";
 
 const server = next({
   dev: isDev,
-  // location of .next generated after running -> yarn build
-  conf: {distDir: path.join(__dirname, ".next")},
+  conf: { distDir: path.join(__dirname, ".next") },
 });
 
 const nextjsHandle = server.getRequestHandler();
+
 exports.nextServer = https.onRequest((req, res) => {
-  return server.prepare().then(() => {
-    return nextjsHandle(req, res);
-  });
+  return server.prepare().then(() => nextjsHandle(req, res));
 });
