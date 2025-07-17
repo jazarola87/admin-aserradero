bb8481115dc76dcde674d75b68bd77b4fdcf4ac0
Javier Azarola <jazarola87@gmail.com>
2025-07-17 12:10:52 +0000

    esto esta bien?

--- next.config.js
+++ next.config.js
@@ -19,16 +19,6 @@ const withPWA = require('next-pwa')({
   ],
 });
 
-const nextConfig = {
-  env: {
-    NEXT_PUBLIC_FIREBASE_API_KEY: "AIzaSyD-NVeAE1XPsQxrQWvMo8XDfNDpvK4YT0o",
-    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: "aserradero-lhm-336e9.firebaseapp.com",
-    NEXT_PUBLIC_FIREBASE_PROJECT_ID: "aserradero-lhm-336e9",
-    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: "aserradero-lhm-336e9.appspot.com",
-    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: "913382798289",
-    NEXT_PUBLIC_FIREBASE_APP_ID: "1:913382798289:web:96e8e815e10398d5c41496",
-    NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: "G-RFR653K87N"
-  },
-};
+const nextConfig = {};
 
 module.exports = withPWA(nextConfig);
