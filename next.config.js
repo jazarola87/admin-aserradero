94f96c3e8662446ffc30261d5af830edd89bdb0b
Javier Azarola <jazarola87@gmail.com>
2025-07-17 11:29:59 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -20,7 +20,15 @@ const withPWA = require('next-pwa')({
 });
 
 const nextConfig = {
-  // Tu configuración de Next.js existente va aquí
+  env: {
+    NEXT_PUBLIC_FIREBASE_API_KEY: "TU_API_KEY_AQUI",
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: "aserradero-lhm-336e9.firebaseapp.com",
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: "aserradero-lhm-336e9",
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: "aserradero-lhm-336e9.appspot.com",
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: "913382798289",
+    NEXT_PUBLIC_FIREBASE_APP_ID: "1:913382798289:web:96e8e815e10398d5c41496",
+    NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: "G-RFR653K87N"
+  },
 };
 
 module.exports = withPWA(nextConfig);
